dcba48c54c41ed8424a719f64d88e7213e319c8a
petuhovskiy <petuhovskiy@yandex.ru>
2019-01-19 23:28:24 +0300

    Remove after delay

--- front/index.js
+++ front/index.js
@@ -4,32 +4,48 @@ const msgs = require('./msg')
 module.exports = bot => msg => {
     console.log("Outgoing message:", msg)
     if (msg.limit == 3) {
-        bot.sendHTML(msg.chatId, msgs.errorSelfLike())
+        utils.deleteLater(
+            bot.sendHTML(msg.chatId, msgs.errorSelfLike())
+        )
         return
     }
     if (msg.limit == 4) {
-        bot.sendHTML(msg.chatId, msgs.errorAlreadyLikedUser())
+        utils.deleteLater(
+            bot.sendHTML(msg.chatId, msgs.errorAlreadyLikedUser())
+        )
         return
     }
     if (msg.limit == 2) {
-        bot.sendHTML(msg.chatId, msgs.errorDailyUpLimit())
+        utils.deleteLater(
+            bot.sendHTML(msg.chatId, msgs.errorDailyUpLimit())
+        )
         return
     }
     if (msg.limit == 1) {
-        bot.sendHTML(msg.chatId, msgs.errroDailyDownLimit())
+        utils.deleteLater(
+            bot.sendHTML(msg.chatId, msgs.errroDailyDownLimit())
+        )
         return
     }
     if (msg.limit == 5) {
-        bot.sendHTML(msg.chatId, msgs.errorCantUp())
+        utils.deleteLater(
+            bot.sendHTML(msg.chatId, msgs.errorCantUp())
+        )
         return
     }
     if (msg.limit == 6) {
-        bot.sendHTML(msg.chatId, msgs.errorInvalidUsername())
+        utils.deleteLater(
+            bot.sendHTML(msg.chatId, msgs.errorInvalidUsername())
+        )
         return
     }
     if (msg.type == "stats") {
-        bot.sendHTML(msg.chatId, msgs.getStatsMessage(msg.result, msg.page))
+        utils.deleteLater(
+            bot.sendHTML(msg.chatId, msgs.getStatsMessage(msg.result, msg.page))
+        )
         return
     }
-    bot.sendHTML(msg.chatId, msgs.getReputationInfo(msg.reputation))
+    utils.deleteLater(
+        bot.sendHTML(msg.chatId, msgs.getReputationInfo(msg.reputation))
+    )
 }
